5660b17a972dd2fc77a765eca2ea2ff9c3dd3f0c
viiniseven7 <viinioliveira458@gmail.com>
2025-07-24 17:28:37 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    $host= 'meu-postgres';
+    $port= '5432';
+    $dbname= 'Biblioteca'
+    $user= 'vinicius';
+    $pass= 'vinicius';
+
+    $dsn= "pgsql:host=$host;port=$port;dbname=$dbname";
+
+    $options = [
+
+        PDO ::
+    ]
+
+
+
+
+?>
